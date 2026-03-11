94afc570ad9ecd50032f449fefec54ebda84c5f3
DianaBianca <dianabiancasr@gmail.com>
2026-03-10 23:16:25 -0300

    ajustes bd

--- output.tf
+++ output.tf
@@ -7,3 +7,7 @@ output "db_instance_port" {
   description = "A porta de conexão da instância RDS."
   value       = aws_db_instance.db_instance.port
 }
+
+output "redis_host" {
+  value = aws_elasticache_cluster.redis.cache_nodes[0].address
+}
